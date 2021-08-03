557377526c570c7e449826192f23cdedaa97f242
Yoann Anthony <yoann.anthony@protonmail.ch>
2021-08-03 07:10:24 +0200

    Final submission

--- prj-001_la-calculatrice/01-sources/main.py
+++ prj-001_la-calculatrice/01-sources/main.py
@@ -1,3 +1,7 @@
 # Demander à l'utilisateur d'entrer un premier nombre
+a = input("Veuillez entrer un premier nombre : ")
 # Demander à l'utilisateur d'entrer un deuxième nombre
+b = input("Veuillez entrer un deuxième nombre : ")
+resultat = int(a) + int(b)
 # Afficher à l'écran le résultat de l'addition (exemple : 'Le résultat de l'addition de 5 + 10 est égal à 15')
+print(f"Le résultat de l'addition de {a} + {b} est égal à {resultat}")
