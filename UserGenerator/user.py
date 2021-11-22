0a77221b9682f7376e005a3b18c074be644d5eff
Yoann Anthony <yoann.anthony@protonmail.ch>
2021-11-22 21:42:26 +0100

    adding documentation and logging

--- UserGenerator/user.py
+++ UserGenerator/user.py
@@ -1,12 +1,35 @@
+"""Module to generate random users"""
 import faker
+import logging
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent
+logging.basicConfig(filename=BASE_DIR / "user.log", level=logging.INFO)
 
 fake = faker.Faker("fr_CH")
 
+
 def get_user():
+  """Generate a Single user
+
+  Returns:
+      [str]: [user]
+  """
+  logging.info("Generating user.")
   return f"{fake.first_name()} {fake.last_name()}"
 
+
 def get_users(n):
-  
+  """[Generate a list of users]
+
+  Args:
+      n ([int]): [number of user to generate]
+
+  Returns:
+      list[str]: [users]
+  """
+  logging.info(f"Generating a list of {n} user.")
   # avec une comrehension de liste
   return[get_user() for _ in range(n)]
   
