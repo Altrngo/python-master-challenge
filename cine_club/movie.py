186b196c360dbe36b10b1253e6a4577a219f2c65
Yoann Anthony <yoann.anthony@protonmail.ch>
2021-12-08 09:31:25 +0100

    add remove method to movies class

--- cine_club/movie.py
+++ cine_club/movie.py
@@ -1,23 +1,52 @@
 import os
-
+import json
+import logging
 
 CUR_DIR = os.path.dirname(__file__)
 DATA_FILE = os.path.join(CUR_DIR, "data", "movies.json")
-print(DATA_FILE)
+# print(DATA_FILE)
 
 class Movie:
   def __init__(self, title):
     self.title = title.title()
+
   
   def __str__(self):
     return self.title
   
   def _get_movies(self):
-    pass
+    with open(DATA_FILE, "r") as f:
+      return json.load(f)
+      
   
-  def _write_movies(self):
-    pass
+  def _write_movies(self, movies):
+    with open(DATA_FILE, "w") as f:
+      json.dump(movies, f, indent=4)
   
-if __name__ == "__main__":    
+  def add_to_movies(self):
+    # Recuperer la liste des films
+    movies = self._get_movies()
+    
+    # Verifier que le film n'est pas deja dans la liste
+    if self.title not in movies:
+      movies.append(self.title)
+    # Si ce n'est pas le cas on l'ajoute
+      self._write_movies(movies)
+      return True
+    # Si c'est le cas, on affiche un message pour indiquer que le film est deja present (avec le module logging.)
+    else:
+      logging.warning(f"Le film {self.title} est déjà enregistré.")
+      return False
+
+  
+  def remove_from_movies(self):
+    movies = self._get_movies()
+    
+    if self.title in movies:
+      movies.remove*(self.title)
+      self._write_movies(movies)
+      
+  
+if __name__ == "__main__":
   m = Movie("harry potter")
-  print(m)
+  m.add_to_movies()
